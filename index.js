edb9ef3a0d4fd67bdd908abb38b134df86751b2b
saronica83 <sarodriguez83@hotmail.com>
2018-03-13 02:54:51 -0600

    Agregando mi profesión

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log("Hola mundo saronica");
 
 var x={
     name:"Programando con ayuda de Dios 2018",
-    birthPlace:"Managua Nicaragua"
+    birthPlace:"Managua Nicaragua",
+    profesion:"Super Ingeniero en computación"
 }
 
-console.log(x.name + " " + x.birthPlace);
+console.log(x.name + " " + x.birthPlace + " " + x.profesion);
