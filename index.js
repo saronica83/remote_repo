1ad6c4cdd78011ab75ee09c2f3552e2168fc5f69
saronica83 <sarodriguez83@hotmail.com>
2018-03-14 06:17:59 -0600

    Agregue mi estado civil

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ console.log("Hola mundo saronica");
 var x={
     name:"Programando con ayuda de Dios 2018",
     birthPlace:"Managua Nicaragua",
-    profesion:"Super Ingeniero en computación"
+    profesion:"Super Ingeniero en computación",
+    estado:"Casdo con Suley Lopez Caldera"
 }
 
 console.log(x.name + " " + x.birthPlace + " " + x.profesion);
